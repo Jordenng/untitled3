2933f5c2f1ca923ad11d8319b64faff29021b9ef
Yarden Nagel <nagel.yarden@gmail.com>
2021-07-18 16:50:53 +0300

    Initial commit

---
+++ callstack.js
@@ -0,0 +1,33 @@
+const fakePromise=(url)=>{
+    return new Promise((resolve, reject) => {
+        const rand = Math.random();
+            setTimeout(()=>{
+                if (rand<0.7){
+                    resolve();
+                } reject("error")
+            },1000)
+        }
+    )
+}
+
+fakePromise('/dogs/1')
+    .then((data)=> {
+        console.log('done');
+        console.log("data is", data);
+    }).catch((err)=>{
+        console.log("ohno",err);
+})
+
+const delayedColorChange = (color, delay)=>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            document.body.style.backgroundColor=color;
+            resolve();
+        },delay)
+    })
+}
+
+delayedColorChange('red',1000)
+    .then(()=> delayedColorChange('orange',1000))
+    .then(()=> delayedColorChange('green',1000)
+    )
